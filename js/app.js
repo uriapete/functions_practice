2effd31ed57657538a15ea80d1a431c9c301edeb
uriapete <uriarteangelo@gmail.com>
2023-02-04 07:00:38 -0800

    Completed Problem 2

--- js/app.js
+++ js/app.js
@@ -13,3 +13,25 @@ function printGreeting(name) {
 // console.log(printGreeting("Slimer"));
 // all good, OK
 
+// 2. reverseWordOrder
+
+// Write a function reverseWordOrder that accepts a single argument, a string. The function should return a string with the order of the words reversed. Don’t worry about punctuation.
+
+function reverseWordOrder(sentence) {
+    let reverseSentence = ' ';
+    let sentArr = sentence.split(' ');
+    for (let i = sentArr.length - 1; i >= 0; i--) {
+        const element = sentArr[i];
+        if (i===0) {reverseSentence += element;}
+        else {reverseSentence += element + ' ';}
+    }
+    return reverseSentence;
+}
+
+// console.log(reverseWordOrder("Ishmael me Call"));
+
+//     => "Call me Ishmael"
+
+// console.log(reverseWordOrder("I use Lâncome on my comb"));
+
+//     => "comb my on Lâncome use I"
