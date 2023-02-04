a81fbd33adb38d9f59cb42ed0bdf22c469eff937
uriapete <uriarteangelo@gmail.com>
2023-02-04 06:49:29 -0800

    Completed Problem 1

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// 1. printGreeting
+
+// Write a function called printGreeting with a parameter name that returns a greeting with the argument interpolated into the greeting.
+
+// console.log(printGreeting("Slimer"));
+
+//     => Hello there, Slimer!
+
+function printGreeting(name) {
+    return `Hello there, ${name}!`;
+}
+
+// console.log(printGreeting("Slimer"));
+// all good, OK
+
